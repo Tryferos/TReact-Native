3f488c6f61860efe883da28352aa31fbaa488350
Tryferos <truf022@gmail.com>
2024-07-15 17:03:03 +0300

    rename assets to values constants and rebrand keys

--- src/constants/values.ts
+++ src/constants/values.ts
@@ -1,12 +1,14 @@
 const colors = Object.freeze({
   black: '#000000',
   white: '#FFFFFF',
+  gray: '#555555',
+  transparent: 'transparent',
 });
 
 const fontSize = Object.freeze({
-  '2xs': 10,
-  xs: 11,
-  xSm: 12,
+  '3xs': 10,
+  '2xs': 11,
+  xs: 12,
   sm: 14,
   md: 16,
   lg: 18,
@@ -17,9 +19,9 @@ const fontSize = Object.freeze({
 });
 
 const iconSize = Object.freeze({
-  '2xs': 8,
-  xs: 12,
-  xSm: 16,
+  '3xs': 8,
+  '2xs': 12,
+  xs: 16,
   sm: 20,
   md: 24,
   lg: 32,
@@ -28,17 +30,33 @@ const iconSize = Object.freeze({
   '3xl': 96,
 });
 
+const gapSize = Object.freeze({
+  zero: 0,
+  '3xs': 2,
+  '2xs': 4,
+  xs: 8,
+  sm: 12,
+  md: 16,
+  lg: 20,
+  xl: 24,
+  '2xl': 32,
+  '3xl': 48,
+});
+
 type Colors = keyof typeof colors;
 type FontSize = keyof typeof fontSize;
 type IconSize = keyof typeof iconSize;
+type GapSize = keyof typeof gapSize;
 
 export {
   //* Values to use
   colors as AppColors,
   fontSize as AppFontSize,
   iconSize as AppIconSize,
+  gapSize as AppGapSize,
   //* Types shortcut
   type Colors as ColorsType,
   type FontSize as FontSizeType,
   type IconSize as IconSizeType,
+  type GapSize as GapSizeType,
 };
