a75003dc52ff1626d9202b5a5af70f6dba28d018
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-08-03 14:54:06 +0300

    make column and row components clickable

--- src/components/elements/Column.tsx
+++ src/components/elements/Column.tsx
@@ -1,23 +1,40 @@
 import {FC, PropsWithChildren} from 'react';
-import {StyleProp, View, ViewProps, ViewStyle} from 'react-native';
-import {AppColors, AppGapSize, GapSizeType} from '../../constants/values';
+import {
+  StyleProp,
+  TouchableOpacity,
+  View,
+  ViewProps,
+  ViewStyle,
+} from 'react-native';
+import {AppColors, AppSpace} from '../../constants/values';
 import {BoxProps} from '../../types/components';
+import {shadowStyles} from './Row';
 
 export const Column: FC<BoxProps> = ({
   gap = 'zero',
   backgroundColor = 'transparent',
-  ...rest
+  children,
+  style: extraStyles,
+  onPress,
+  shadow,
 }) => {
-  const extraStyles = (rest.style ?? []) as [];
-  return (
-    <View
-      {...rest}
-      style={[
-        {gap: AppGapSize[gap], backgroundColor: AppColors[backgroundColor]},
-        ...extraStyles,
-      ]}
-      className={`flex flex-col`}>
-      {rest.children}
-    </View>
-  );
+  const styles = [
+    {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: AppSpace[gap],
+      backgroundColor: AppColors[backgroundColor],
+      ...shadowStyles[shadow ?? 'undefined'],
+    },
+    extraStyles,
+  ] as ViewStyle[];
+
+  if (onPress) {
+    return (
+      <TouchableOpacity onPress={onPress} activeOpacity={1} style={styles}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+  return <View style={styles}>{children}</View>;
 };
--- src/components/elements/Row.tsx
+++ src/components/elements/Row.tsx
@@ -1,23 +1,78 @@
 import {FC, PropsWithChildren} from 'react';
-import {View, ViewProps, ViewStyle} from 'react-native';
-import {AppColors, AppGapSize, GapSizeType} from '../../constants/values';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  ViewProps,
+  ViewStyle,
+} from 'react-native';
+import {AppColors, AppSpace} from '../../constants/values';
 import {BoxProps} from '../../types/components';
 
 export const Row: FC<BoxProps> = ({
   gap = 'zero',
   backgroundColor = 'transparent',
-  ...rest
+  children,
+  shadow,
+  onPress,
+  style: extraStyles,
 }) => {
-  const extraStyles = (rest.style ?? []) as [];
-  return (
-    <View
-      {...rest}
-      style={[
-        {gap: AppGapSize[gap], backgroundColor: AppColors[backgroundColor]},
-        ...extraStyles,
-      ]}
-      className={`flex flex-row`}>
-      {rest.children}
-    </View>
-  );
+  const styles = [
+    {
+      display: 'flex',
+      flexDirection: 'row',
+      gap: AppSpace[gap],
+      backgroundColor: AppColors[backgroundColor],
+      ...shadowStyles[shadow ?? 'undefined'],
+    },
+    extraStyles,
+  ] as ViewStyle[];
+
+  if (onPress) {
+    return (
+      <TouchableOpacity onPress={onPress} activeOpacity={1} style={styles}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+  return <View style={styles}>{children}</View>;
 };
+
+export const shadowStyles = StyleSheet.create({
+  undefined: {},
+  shadowSmall: {
+    shadowColor: 'rgba(50, 50, 93, 0.3)',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 1,
+    shadowRadius: 100,
+    elevation: 10, // This is for Android shadow
+    marginHorizontal: AppSpace['2xs'],
+    marginVertical: AppSpace['3xs'],
+  },
+  shadowMedium: {
+    shadowColor: 'rgba(0, 0, 0, 0.5)',
+    shadowOffset: {width: 0, height: 14},
+    shadowOpacity: 1,
+    shadowRadius: 28,
+    elevation: 10,
+    marginHorizontal: AppSpace['2xs'],
+    marginVertical: AppSpace['3xs'],
+  },
+  shadowMediumLight: {
+    shadowColor: 'rgba(0, 0, 0, 0.25)',
+    shadowOffset: {width: 0, height: 14},
+    shadowOpacity: 1,
+    shadowRadius: 28,
+    elevation: 10,
+    marginHorizontal: AppSpace['2xs'],
+    marginVertical: AppSpace['3xs'],
+  },
+  shadowBottom: {
+    shadowColor: 'rgba(0, 0, 0, 0.25)',
+    shadowOffset: {width: 0, height: 14},
+    shadowOpacity: 1,
+    shadowRadius: 14,
+    elevation: 10,
+  },
+});
+//box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
