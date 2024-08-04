ec2a5abb572416364a2116bde62fec2f9091ad79
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-08-04 13:09:25 +0300

    [Components] Change RoundButton with new Row implementation

--- src/components/organisms/RoundButton.tsx
+++ src/components/organisms/RoundButton.tsx
@@ -1,30 +1,49 @@
 import {FC} from 'react';
-import {TouchableOpacity, ViewStyle} from 'react-native';
+import {TextStyle, TouchableOpacity, ViewStyle} from 'react-native';
 import {Row} from '../elements/Row';
 import {CustomText} from '../elements/CustomText';
-import {AppBoxSize} from '../../constants/values';
+import {AppSpace, ColorsType} from '../../constants/values';
+import {Column} from '../elements/Column';
+import {AppIconsType} from '../../types/icons';
+import {CustomIcon} from '../elements/CustomIcon';
+import {Spacer} from '../elements/Spacer';
 
 type RoundButtonProps = {
   onPress?: () => void;
   text: string;
   style?: ViewStyle;
+  textColor?: ColorsType;
+  disabled?: boolean;
+  icon?: AppIconsType;
 };
 
-export const RoundButton: FC<RoundButtonProps> = ({onPress, text, style}) => {
+export const RoundButton: FC<RoundButtonProps> = ({
+  onPress,
+  text,
+  style,
+  textColor,
+  icon,
+  disabled = false,
+}) => {
   return (
-    <TouchableOpacity onPress={onPress} style={{flex: 1}}>
-      <Row
-        style={{width: '100%', paddingVertical: AppBoxSize.xs, ...style}}
-        className="px-6 rounded-xl justify-center items-center"
-        backgroundColor="main">
-        <CustomText
-          onPress={onPress}
-          color="white"
-          font="wotfardMedium"
-          size="md">
-          {text}
-        </CustomText>
-      </Row>
-    </TouchableOpacity>
+    <Row
+      onPress={disabled ? undefined : onPress}
+      style={{
+        opacity: disabled ? 0.5 : 1,
+        width: '100%',
+        justifyContent: icon ? 'space-between' : 'center',
+        paddingVertical: AppSpace.xs,
+        ...style,
+      }}
+      className="px-6 rounded-xl justify-center items-center"
+      backgroundColor="main">
+      {icon && <Row />}
+      <CustomText color={textColor ?? 'white'} font="wotfardMedium" size="md">
+        {text}
+      </CustomText>
+      {icon && (
+        <CustomIcon icon={icon} size="xs" color={textColor ?? 'white'} />
+      )}
+    </Row>
   );
 };
