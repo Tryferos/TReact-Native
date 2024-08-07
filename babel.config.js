243ba8477c952da2d224cea7368747f1ed2b5eb5
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-08-07 17:52:24 +0300

    [React Native Configs] Update metro and babel config files

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,25 @@ module.exports = {
   plugins: [
     'nativewind/babel',
     [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    [
       'babel-plugin-inline-import',
       {
         extensions: ['.svg'],
       },
     ],
+    'react-native-reanimated/plugin',
   ],
 };
+``;
