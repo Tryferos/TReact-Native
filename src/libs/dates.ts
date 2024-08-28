adb10286543fe6ca478935659142d0cd08bc41be
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-08-28 14:55:49 +0300

    date function helpers

--- src/libs/dates.ts
+++ src/libs/dates.ts
@@ -11,9 +11,57 @@ const formatNumber = (number: number) => {
   return `${number}th`;
 };
 export const getDateString = (date: Date) => {
-  const isToday = date.getDate() === new Date().getDate();
+  const agoString = getDateStringAgo(date);
   const day = formatNumber(date.getDate());
   const dayName = date.toLocaleString('en-US', {weekday: 'long'});
   const monthName = date.toLocaleString('en-US', {month: 'short'});
-  return [isToday, `${dayName}, ${day} ${monthName}`];
+  return [agoString, `${dayName}, ${day} ${monthName}`];
+};
+
+export const getDateStringAgo = (_date: Date) => {
+  const date = getDateDayStart(_date);
+  const currentDate = getDateDayStart(new Date());
+  const isToday = date.getDate() === currentDate.getDate();
+  if (isToday) {
+    return 'Today';
+  }
+  const singleDayMs = 1000 * 60 * 60 * 24;
+  const ago = currentDate.getTime() - date.getTime();
+  if (ago <= singleDayMs) {
+    return 'Yesterday';
+  }
+  if (ago <= singleDayMs * 7) {
+    return 'This week';
+  }
+  if (ago <= singleDayMs * 14) {
+    return 'Last week';
+  }
+  if (ago <= singleDayMs * 30) {
+    return 'This month';
+  }
+  if (ago <= singleDayMs * 60) {
+    return 'Last month';
+  }
+  if (ago <= singleDayMs * 365) {
+    return 'This year';
+  }
+  return 'Last year';
+};
+
+export const getDatePickerDate = (_date: Date) => {
+  const date = getDateDayStart(_date);
+  const day = date.getDate();
+  const dayName = date.toLocaleString('en-US', {weekday: 'narrow'});
+  return {day, dayName};
+};
+
+export const getDateDayStart = (date: Date) => {
+  date.setHours(0, 0, 0, 0);
+  return date;
+};
+
+export const getDateByIndex = (index: number) => {
+  const date = new Date();
+  date.setDate(date.getDate() - index);
+  return getDateDayStart(date);
 };
